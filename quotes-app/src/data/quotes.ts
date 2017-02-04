ea14e73eceff98bd7c404a785374a9a9a06439a0
Aleksanteri <alrytkonen@gmail.com>
2017-02-04 14:06:42 +0200

    training

---
+++ quotes-app/src/data/quotes.ts
@@ -0,0 +1,71 @@
+export default [
+    {
+        category: 'inspirational',
+        quotes: [
+            {
+                id: '1',
+                person: 'James "Mad Dog" Mattis',
+                text: 'The first time you blow someone away is not an insignificant event. That said, there are some a--holes in the world that just need to be shot. There are hunters and there are victims. By your discipline, you will decide if you are a hunter or a victim.'
+            },
+            {
+                id: '2',
+                person: 'James "Mad Dog" Mattis',
+                text: 'Be polite, be professional, but have a plan to kill everybody you meet.'
+            },
+            {
+                id: '3',
+                person: 'James "Mad Dog" Mattis',
+                text: "I come in peace. I didn’t bring artillery. But I’m pleading with you, with tears in my eyes: If you fuck with me, I’ll kill you all."
+            }
+        ],
+        icon: 'brush'
+    },
+    {
+        category: 'ability',
+        quotes: [
+            {
+                id: '4',
+                person: 'John Wooden',
+                text: 'Ability may get you to the top, but it takes character to keep you there.'
+            },
+            {
+                id: '5',
+                person: 'Robert Frost',
+                text: 'Education is the ability to listen to almost anything without losing your temper.'
+            }
+        ],
+        icon: 'bicycle'
+    },
+    {
+        category: 'enthusiasm',
+        quotes: [
+            {
+                id: '6',
+                person: 'Benjamin Disraeli',
+                text: 'Every product of genius must be the product of enthusiasm.'
+            },
+            {
+                id: '7',
+                person: 'Norman Vincent Peale',
+                text: 'Enthusiasm releases the drive to carry you over obstacles and adds significance to all you do.'
+            }
+        ],
+        icon: 'battery-charging'
+    },
+    {
+        category: 'motivational',
+        quotes: [
+            {
+                id: '8',
+                person: 'Jim Rohn',
+                text: 'Either you run the day or the day runs you.'
+            },
+            {
+                id: '9',
+                person: 'Donna Brazile',
+                text: 'I was motivated to be different in part because I was different.'
+            }
+        ],
+        icon: 'body'
+    }
+];
